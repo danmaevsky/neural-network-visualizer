cc759d49eeff7cb9ff6a115000b8023d1a94198b
Daniel Maevsky <dan.mayevskiy@gmail.com>
2022-06-25 12:53:00 -0400

    Fixed a update() bug in useArray hook that has been around since kspline project. Created tentative components directory (subject to change)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import useArray from "./hooks/useArray";
 
 function App() {
+  let [arr, arrMethods] = useArray(["a", "b", "c"]);
+
+  let loopExample = () => {
+    for (let i = 0; i < arr.length; i++) {
+      arrMethods.update(i, 2 * i + 1);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +19,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        {arr}
+        <button onClick={loopExample}>Click to run loopExample</button>
       </header>
     </div>
   );
